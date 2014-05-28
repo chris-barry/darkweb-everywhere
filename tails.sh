004e9f0d6de92c674768e86c4cf85f283b0fc289
Colin Mahns <goatman93@gmail.com>
2014-05-28 01:57:04 +0000

    Update tails.sh
    And this is why we don't sample the medicine cabinet.

--- tails.sh
+++ tails.sh
@@ -5,6 +5,6 @@ TAILS=".mozilla/firefox/default/HTTPSEverywhereUserRules/"
 GREEN='\e[0;32m'
 NC='\e[0m' # No color - include this otherwise your terminal output gets messed up.
 
-mkdir -p $HOME/$TORBROWSER # makes $TORBROWSER directory if none exists.
+mkdir -p $HOME/$TAILS # makes $TORBROWSER directory if none exists.
 cp rules/*.xml $HOME/$TAILS # could also be ~/
 echo -e "${GREEN}Installation complete!${NC}"
