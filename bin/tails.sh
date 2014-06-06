87fa52d2f89e62694826373aa1857f0dded0fde0
Colin Mahns <colinrmahns@gmail.com>
2014-06-05 21:36:28 -0400

    Updated tails.sh

--- bin/tails.sh
+++ bin/tails.sh
@@ -6,5 +6,5 @@ GREEN='\e[0;32m'
 NC='\e[0m' # No color - include this otherwise your terminal output gets messed up.
 
 mkdir -p $HOME/$TAILS # makes $TORBROWSER directory if none exists.
-cp rules/*.xml $HOME/$TAILS # could also be ~/
+cp ../rules/*.xml $HOME/$TAILS # could also be ~/
 echo -e "${GREEN}Installation complete!${NC}"
